4da838eb7ce413ab48e390d5e1df59aa7e9c4bfb
Pierre Lasante <pierre.lasante@videotron.ca>
2025-03-08 08:22:25 -0500

    Completed AddCategory.js

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -2,26 +2,35 @@ import React, {useState} from 'react';
 import Layout from '../core/Layout';
 import {isAuthenticated} from "../auth";
 import {Link} from 'react-router-dom';
+import {createCategory} from "./apiAdmin";
 
 const AddCategory = () => {
   const [name, setName] = useState('');
-  const [error, setError] = useState(false);
+  const [error, setError] = useState('');
   const [success, setSuccess] = useState(false);
 
   // destructure user and token from localStorage.getItem('jwt')
   const {user, token} = isAuthenticated();
 
   const handleChange = (e) => {
-    setError(false);
+    setError('');
     setName(e.target.value);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setError(false);
+    setError('');
     setSuccess(false);
     // make request to API to create category
-
+    createCategory(user._id, token, {name})
+      .then(data => {
+        if (data.error) {
+          setError(data.error)
+        } else {
+          setError('');
+          setSuccess(true);
+        }
+      })
   }
 
   const newCategoryForm = () => (
@@ -42,13 +51,36 @@ const AddCategory = () => {
     </form>
   )
 
+  const showSuccess = () => {
+    if (success) {
+      return <h3 className={'text-success'}>{name} is successfully created.</h3>
+    }
+  }
+
+  const showError = () => {
+    if (error) {
+      return <h3 className={'text-danger'}>{name} should be unique.</h3>
+    }
+  }
+
+  const goBack = () => (
+    <div className={'mt-5'}>
+      <Link to={'/admin/dashboard'} className={'text-warning'}>
+        Back to Dashboard
+      </Link>
+    </div>
+  )
+
 
   return (
     <Layout title={'Add a new Category'}
             description={`Good Day ${name}, ready to add a new category.`}>
       <div className={'row'}>
         <div className={'col-md-8 offset-md-2 col-xs-12'}>
+          {showSuccess()}
+          {showError()}
           {newCategoryForm()}
+          {goBack()}
         </div>
       </div>
     </Layout>
